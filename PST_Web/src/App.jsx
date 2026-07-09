96c0817386fcb6950fc8196947b24868df3c64ee
cpiles <cpiles@omegacrmconsulting.com>
2026-07-09 20:08:29 +0200

    v2.6.2 - Guard de comision cripto y parcial desactivado en METAL (XAUUSD), TP tecnico RangeBreaker en indices

--- PST_Web/src/App.jsx
+++ PST_Web/src/App.jsx
@@ -1517,7 +1517,7 @@ function App() {
 
 
 
-                <span className="text-zinc-600 font-black font-mono text-[9px] tracking-[0.3em] uppercase">PST-CORE: V2.6.1 SMC</span>
+                <span className="text-zinc-600 font-black font-mono text-[9px] tracking-[0.3em] uppercase">PST-CORE: V2.6.2 SMC</span>
 
 
 
